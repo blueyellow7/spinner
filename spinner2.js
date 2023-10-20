b4d3dcecc2b9dcc945d3e401bd8f298b2286e7a2
Neeharika Baral <neehabaral72@gmail.com>
2023-10-20 17:13:06 -0400

    initial commit

---
+++ spinner2.js
@@ -0,0 +1,9 @@
+const spinnerArray = ['\r|', '\r/', '\r-', '\r\\', '\r|', '\r/', '\r-', '\r\\'];
+
+let delay = 0;
+for (const char of spinnerArray) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay)
+  delay += 200;
+};
